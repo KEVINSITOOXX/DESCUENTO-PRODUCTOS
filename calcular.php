1ee169078245e94181596e16fda5855e3f3285d8
KEVINSITOOXX <kvivas799@gmail.com>
2025-09-13 10:26:10 -0500

    Update calcular.php

--- calcular.php
+++ calcular.php
@@ -17,25 +17,4 @@
     $total_a_pagar = $subtotal - $descuento;
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Resultado del Cálculo</title>
-</head>
-<body>
-    <h2>Resultados</h2>
 
-    <h3>Productos Comprados:</h3>
-    <ul>
-        <li><?php echo "Nombre: $nombre1, Precio: $precio1"; ?></li>
-        <li><?php echo "Nombre: $nombre2, Precio: $precio2"; ?></li>
-        <li><?php echo "Nombre: $nombre3, Precio: $precio3"; ?></li>
-    </ul>
-
-    <p>La suma total de los precios es: $<?php echo number_format($subtotal, 2); ?></p>
-    <p>Descuento aplicado (16%): $<?php echo number_format($descuento, 2); ?></p>
-    <h3>Total a Pagar: $<?php echo number_format($total_a_pagar, 2); ?></h3>
-
-</body>
-</html>
